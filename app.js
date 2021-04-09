dc13a43429df203c5da291fd939cd1a425fb3d4d
Shrey Agarwal <42926487+shreyagarwal13@users.noreply.github.com>
2021-04-09 23:24:58 +0530

    Add files via upload
    Initial Commit

---
+++ app.js
@@ -0,0 +1,206 @@
+const testItem = document.getElementById("textDisplay");
+const inputItem = document.getElementById("textInput");
+const timeName = document.getElementById("timeName");
+const time = document.getElementById("time");
+const cwName = document.getElementById("cwName");
+const cw = document.getElementById("cw");
+const restartBtn = document.getElementById("restartBtn");
+const thirty = document.getElementById("thirty");
+const sixty = document.getElementById("sixty");
+
+var wordNo = 1;
+var wordsSubmitted = 0;
+var wordsCorrect = 0;
+var timer = 30;
+var flag=0;
+var factor=2;
+var seconds;
+
+displayTest();
+
+
+
+//on Input
+inputItem.addEventListener('input', function(event){
+  if(flag===0){
+    flag=1;
+    timeStart();
+  }
+  var charEntered = event.data;
+  if(charEntered===' '){
+    checkWord();
+  }
+  else{
+    currentWord();
+  }
+});
+
+//time selection
+thirty.addEventListener("click",function(){
+  timer = 30;
+  factor = 2;
+  thirty.classList.add('yellow');
+  sixty.classList.remove('yellow');
+  time.innerText = timer;
+});
+
+sixty.addEventListener("click",function(){
+  timer = 60;
+  factor = 1;
+  sixty.classList.add('yellow');
+  thirty.classList.remove('yellow');
+  time.innerText = timer;
+});
+
+//restart the Test
+restartBtn.addEventListener("click",function(){
+  wordsSubmitted = 0;
+  wordsCorrect = 0;
+  flag=0;
+
+  time.classList.remove("current");
+  cw.classList.remove("current");
+  time.innerText = timer;
+  timeName.innerText = "Time";
+  cw.innerText = wordsCorrect;
+  cwName.innerText = "CW";
+
+  displayTest();
+  inputItem.disabled = false;
+  inputItem.value = '';
+  inputItem.focus();
+  clearInterval(seconds);
+});
+
+
+
+//start the timer countdown
+function timeStart(){
+  thirty.style.visibility = 'hidden';
+  sixty.style.visibility = 'hidden';
+
+  seconds = setInterval(function() {
+    time.innerText--;
+    if (time.innerText == "-1") {
+        timeOver();
+        clearInterval(seconds);
+    }
+  }, 1000);
+}
+
+//diable textarea and wait for restart
+function timeOver(){
+  inputItem.disabled = true;
+  thirty.style.visibility = 'visible';
+  sixty.style.visibility = 'visible';
+  restartBtn.focus();
+  displayScore();
+}
+
+//display the score
+function displayScore(){
+  let percentageAcc = 0;
+  if(wordsSubmitted!==0){
+    percentageAcc = Math.floor((wordsCorrect/wordsSubmitted)*100);
+  }
+  time.classList.add("current");
+  cw.classList.add("current");
+
+  time.innerText = percentageAcc+"%";
+  timeName.innerText = "PA";
+
+  cw.innerText = factor*wordsCorrect;
+  cwName.innerText = "WPM";
+}
+
+//check if the user is entering correcrt word
+function currentWord(){
+  const wordEntered = inputItem.value;
+  const currentID = "word "+wordNo;
+  const currentSpan = document.getElementById(currentID);
+  const curSpanWord = currentSpan.innerText;
+
+  if(wordEntered == curSpanWord.substring(0,wordEntered.length)){
+    colorSpan(currentID, 2);
+  }
+  else{
+    colorSpan(currentID, 3);
+  }
+
+}
+//checks word entered
+function checkWord(){
+  const wordEntered = inputItem.value;
+  inputItem.value='';
+
+  const wordID = "word "+wordNo;
+  const checkSpan = document.getElementById(wordID);
+  wordNo++;
+  wordsSubmitted++;
+
+  if(checkSpan.innerText === wordEntered){
+    colorSpan(wordID, 1);
+    wordsCorrect++;
+    cw.innerText=wordsCorrect;
+  }
+  else{
+    colorSpan(wordID, 3);
+  }
+
+  if(wordNo>40){
+
+    displayTest();
+  }
+  else{
+    const nextID = "word "+wordNo;
+    colorSpan(nextID, 2);
+  }
+}
+
+//color the words
+function colorSpan(id, color){
+  const span = document.getElementById(id);
+  if(color === 1 ){
+    span.classList.remove('wrong');
+    span.classList.remove('current');
+    span.classList.add('correct');
+  }
+  else if(color ===2){
+    span.classList.remove('correct');
+    span.classList.remove('wrong');
+    span.classList.add('current');
+  }
+  else{
+    span.classList.remove('correct');
+    span.classList.remove('current');
+    span.classList.add('wrong');
+  }
+}
+
+//display the random words on screen
+function displayTest(){
+  wordNo = 1;
+  let newTest = randomWords();
+  testItem.innerHTML = '';
+  newTest.forEach(function(word, i){
+    let wordSpan = document.createElement('span');
+    wordSpan.innerText = word;
+    wordSpan.setAttribute("id", "word " + (i+1));
+    testItem.appendChild(wordSpan);
+  });
+
+  const nextID = "word "+wordNo;
+  colorSpan(nextID, 2);
+}
+
+//Generate an array of random 50 words
+function randomWords(){
+  var topWords = ["a", "ability", "able", "about", "above", "accept", "according", "account", "across", "act", "action", "activity", "actually", "add", "address", "administration", "admit", "adult", "affect", "after", "again", "against", "age", "agency", "agent", "ago", "agree", "agreement", "ahead", "air", "all", "allow", "almost", "alone", "along", "already", "also", "although", "always", "American", "among", "amount", "analysis", "and", "animal", "another", "answer", "any", "anyone", "anything", "appear", "apply", "approach", "area", "argue", "arm", "around", "arrive", "art", "article", "artist", "as", "ask", "assume", "at", "attack", "attention", "attorney", "audience", "author", "authority", "available", "avoid", "away", "baby", "back", "bad", "bag", "ball", "bank", "bar", "base", "be", "beat", "beautiful", "because", "become", "bed", "before", "begin", "behavior", "behind", "believe", "benefit", "best", "better", "between", "beyond", "big", "bill", "billion", "bit", "black", "blood", "blue", "board", "body", "book", "born", "both", "box", "boy", "break", "bring", "brother", "budget", "build", "building", "business", "but", "buy", "by", "call", "camera", "campaign", "can", "cancer", "candidate", "capital", "car", "card", "care", "career", "carry", "case", "catch", "cause", "cell", "center", "central", "century", "certain", "certainly", "chair", "challenge", "chance", "change", "character", "charge", "check", "child", "choice", "choose", "church", "citizen", "city", "civil", "claim", "class", "clear", "clearly", "close", "coach", "cold", "collection", "college", "color", "come", "commercial", "common", "community", "company", "compare", "computer", "concern", "condition", "conference", "congress", "consider", "consumer", "contain", "continue", "control", "cost", "could", "country", "couple", "course", "court", "cover", "create", "crime", "cultural", "culture", "cup", "current", "customer", "cut", "dark", "data", "daughter", "day", "dead", "deal", "death", "debate", "decade", "decide", "decision", "deep", "defense", "degree", "Democrat", "democratic", "describe", "design", "despite", "detail", "determine", "develop", "development", "die", "difference", "different", "difficult", "dinner", "direction", "director", "discover", "discuss", "discussion", "disease", "do", "doctor", "dog", "door", "down", "draw", "dream", "drive", "drop", "drug", "during", "each", "early", "east", "easy", "eat", "economic", "economy", "edge", "education", "effect", "effort", "eight", "either", "election", "else", "employee", "end", "energy", "enjoy", "enough", "enter", "entire", "environment", "environmental", "especially", "establish", "even", "evening", "event", "ever", "every", "everybody", "everyone", "everything", "evidence", "exactly", "example", "executive", "exist", "expect", "experience", "expert", "explain", "eye", "face", "fact", "factor", "fail", "fall", "family", "far", "fast", "father", "fear", "federal", "feel", "feeling", "few", "field", "fight", "figure", "fill", "film", "final", "finally", "financial", "find", "fine", "finger", "finish", "fire", "firm", "first", "fish", "five", "floor", "fly", "focus", "follow", "food", "foot", "for", "force", "foreign", "forget", "form", "former", "forward", "four", "free", "friend", "from", "front", "full", "fund", "future", "game", "garden", "gas", "general", "generation", "get", "girl", "give", "glass", "go", "goal", "good", "government", "great", "green", "ground", "group", "grow", "growth", "guess", "gun", "guy", "hair", "half", "hand", "hang", "happen", "happy", "hard", "have", "he", "head", "health", "hear", "heart", "heat", "heavy", "help", "her", "here", "herself", "high", "him", "himself", "his", "history", "hit", "hold", "home", "hope", "hospital", "hot", "hotel", "hour", "house", "how", "however", "huge", "human", "hundred", "husband", "I", "idea", "identify", "if", "image", "imagine", "impact", "important", "improve", "in", "include", "including", "increase", "indeed", "indicate", "individual", "industry", "information", "inside", "instead", "institution", "interest", "interesting", "international", "interview", "into", "investment", "involve", "issue", "it", "item", "its", "itself", "job", "join", "just", "keep", "key", "kid", "kill", "kind", "kitchen", "know", "knowledge", "land", "language", "large", "last", "late", "later", "laugh", "law", "lawyer", "lay", "lead", "leader", "learn", "least", "leave", "left", "leg", "legal", "less", "let", "letter", "level", "lie", "life", "light", "like", "likely", "line", "list", "listen", "little", "live", "local", "long", "look", "lose", "loss", "lot", "love", "low", "machine", "magazine", "main", "maintain", "major", "majority", "make", "man", "manage", "management", "manager", "many", "market", "marriage", "material", "matter", "may", "maybe", "me", "mean", "measure", "media", "medical", "meet", "meeting", "member", "memory", "mention", "message", "method", "middle", "might", "military", "million", "mind", "minute", "miss", "mission", "model", "modern", "moment", "money", "month", "more", "morning", "most", "mother", "mouth", "move", "movement", "movie", "Mr", "Mrs", "much", "music", "must", "my", "myself", "name", "nation", "national", "natural", "nature", "near", "nearly", "necessary", "need", "network", "never", "new", "news", "newspaper", "next", "nice", "night", "no", "none", "nor", "north", "not", "note", "nothing", "notice", "now", "not", "number", "occur", "of", "off", "offer", "office", "officer", "official", "often", "oh", "oil", "ok", "old", "on", "once", "one", "only", "onto", "open", "operation", "opportunity", "option", "or", "order", "organization", "other", "others", "our", "out", "outside", "over", "own", "owner", "page", "pain", "painting", "paper", "parent", "part", "participant", "particular", "particularly", "partner", "party", "pass", "past", "patient", "pattern", "pay", "peace", "people", "per", "perform", "performance", "perhaps", "period", "person", "personal", "phone", "physical", "pick", "picture", "piece", "place", "plan", "plant", "play", "player", "PM", "point", "police", "policy", "political", "politics", "poor", "popular", "population", "position", "positive", "possible", "power", "practice", "prepare", "present", "president", "pressure", "pretty", "prevent", "price", "private", "probably", "problem", "process", "produce", "product", "production", "professional", "professor", "program", "project", "property", "protect", "prove", "provide", "public", "pull", "purpose", "push", "put", "quality", "question", "quickly", "quite", "race", "radio", "raise", "range", "rate", "rather", "reach", "read", "ready", "real", "reality", "realize", "really", "reason", "receive", "recent", "recently", "recognize", "record", "red", "reduce", "reflect", "region", "relate", "relationship", "religious", "remain", "remember", "remove", "report", "represent", "republican", "require", "research", "resource", "respond", "response", "responsibility", "rest", "result", "return", "reveal", "rich", "right", "rise", "risk", "road", "rock", "role", "room", "rule", "run", "safe", "same", "save", "say", "scene", "school", "science", "scientist", "score", "sea", "season", "seat", "second", "section", "security", "see", "seek", "seem", "sell", "send", "senior", "sense", "series", "serious", "serve", "service", "set", "seven", "several", "sex", "sexual", "shake", "share", "she", "shoot", "short", "shot", "should", "shoulder", "show", "side", "sign", "significant", "similar", "simple", "simply", "since", "sing", "single", "sister", "sit", "site", "situation", "six", "size", "skill", "skin", "small", "smile", "so", "social", "society", "soldier", "some", "somebody", "someone", "something", "sometimes", "son", "song", "soon", "sort", "sound", "source", "south", "southern", "space", "speak", "special", "specific", "speech", "spend", "sport", "spring", "staff", "stage", "stand", "standard", "star", "start", "state", "statement", "station", "stay", "step", "still", "stock", "stop", "store", "story", "strategy", "street", "strong", "structure", "student", "study", "stuff", "style", "subject", "success", "successful", "such", "suddenly", "suffer", "suggest", "summer", "support", "sure", "surface", "system", "table", "take", "talk", "task", "tax", "teach", "teacher", "team", "technology", "television", "tell", "ten", "tend", "term", "test", "than", "thank", "that", "the", "their", "them", "themselves", "then", "theory", "there", "these", "they", "thing", "think", "third", "this", "those", "though", "thought", "thousand", "threat", "three", "through", "throughout", "throw", "thus", "time", "to", "today", "together", "tonight", "too", "top", "total", "tough", "toward", "town", "trade", "traditional", "training", "travel", "treat", "treatment", "tree", "trial", "trip", "trouble", "true", "truth", "try", "turn", "TV", "two", "type", "under", "understand", "unit", "until", "up", "upon", "us", "use", "usually", "value", "various", "very", "victim", "view", "violence", "visit", "voice", "vote", "wait", "walk", "wall", "want", "war", "watch", "water", "way", "we", "weapon", "wear", "week", "weight", "well", "west", "western", "what", "whatever", "when", "where", "whether", "which", "while", "white", "who", "whole", "whom", "whose", "why", "wide", "wife", "will", "win", "wind", "window", "wish", "with", "within", "without", "woman", "wonder", "word", "work", "worker", "world", "worry", "would", "write", "writer", "wrong", "yard", "yeah", "year", "yes", "yet", "you", "young", "your", "yourself"];
+
+  var selectedWords = [];
+  for(var i=0;i<40;i++){
+    var randomNumber = Math.floor(Math.random()*1000);
+    selectedWords.push(topWords[randomNumber]+" ");
+  }
+  return selectedWords;
+}
